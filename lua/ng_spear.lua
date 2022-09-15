8c33a844839e0d7e9f33d0298f0b6235f093dbfb
kbario <kylebario1@gmail.com>
2022-09-15 17:09:24 +0800

    fixed bugs from refactor, added slash support

--- lua/ng_spear.lua
+++ lua/ng_spear.lua
@@ -2,9 +2,19 @@ local spear = require("spear")
 
 M = {}
 
-M.spear_ts = function() spear("component.ts") end
-M.spear_spec = function() spear("component.spec.ts") end
-M.spear_css = function() spear({ "component.css", "component.scss" }) end
-M.spear_html = function() spear("component.html") end
+-- start angular implementations and presets
+-- app specific
+M.spear_mod = function() spear(".module.ts") end
+M.spear_route = function() spear("-routing.module.ts") end
+
+-- component specific
+M.spear_ts = function() spear(".component.ts") end
+M.spear_html = function() spear(".component.html") end
+M.spear_spec = function() spear(".component.spec.ts") end
+M.spear_css = function() spear({ ".component.css", ".component.scss" }) end
+
+-- combos of unlikely pairs
+M.spear_ts_pipe = function() spear({ ".component.ts", ".pipe.ts" }) end
+-- end angular implementations and presets
 
 return M
--- lua/spear.lua
+++ lua/spear.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+local function get_slash(nome)
+  if string.find(nome, "\\") ~= nil then
+    return "\\"
+  elseif string.find(nome, "/") ~= nil then
+    return "/"
+  end
+end
+
 -- start checking functions
 local function is_table(thing)
   return type(thing) == "table"
@@ -11,12 +19,15 @@ end
 
 local function is_valid(extn_input)
   if is_table(extn_input) then
+    local length = 0
     for _, v in ipairs(extn_input) do
       if not is_string(v) then
         print("Please provide only strings")
         return nil
       end
+      length = length + 1
     end
+    if length == 0 then return nil end
     return "table"
   elseif not is_string(extn_input) then
     print("Please provide only strings")
@@ -68,12 +79,12 @@ local function remove_from(to_remove_from, to_remove)
   return string.gsub(to_remove_from, to_remove, "")
 end
 
-local function remove_file_from_dir(dir, file)
-  return remove_from(dir, "/" .. file)
+local function remove_file_from_dir(dir, slash, file)
+  return remove_from(dir, slash .. file)
 end
 
-local function concatenare_sentiero(pathnome, dirnome, extn)
-  return string.format("%s/%s%s", pathnome, dirnome, extn)
+local function concatenare_sentiero(pathnome, slash, dirnome, extn)
+  return string.format("%s%s%s%s", pathnome, slash, dirnome, extn)
 end
 
 -- end string manipulation functions
@@ -84,9 +95,9 @@ local function get_current_name()
   return vim.api.nvim_buf_get_name(buf_id)
 end
 
-local function get_last_sec_of_path(path)
+local function get_last_sec_of_path(path, slash)
   local htap = path:reverse()
-  return string.match(htap, "[^/]*"):reverse()
+  return string.match(htap, "[^" .. slash .. "]*"):reverse()
 end
 
 local function get_buf_to_go_to_id(new_nome)
@@ -97,40 +108,48 @@ local function get_buf_to_go_to_id(new_nome)
   end
 end
 
-local function get_ext_as_string(ext_inpt)
-  if is_string(ext_inpt) then return ext_inpt end
+local function get_ext_as_string(dirnome, ext_inpt)
   local ext_string = ""
-  local idx = 0
-  local length = 0
-  for _, _ in pairs(ext_inpt) do
-    length = length + 1
-  end
-  for _, v in pairs(ext_inpt) do
-    local sep
-    if idx == 0 then
-      sep = " "
-    elseif idx == length-1 then
-      sep = " or "
-    else
-      sep = ", "
+  if is_string(ext_inpt) then
+    ext_string = string.format("%s%s", dirnome, ext_inpt)
+    return ext_string
+  elseif is_table(ext_inpt) then
+    local idx = 0
+    local length = 0
+    for _, _ in pairs(ext_inpt) do
+      length = length + 1
     end
-    ext_string = string.format("%s%s%s", ext_string, sep, v)
-    idx = idx+1
+    if length == 0 then
+      return nil
+    end
+
+    for _, v in pairs(ext_inpt) do
+      local sep
+      idx = idx + 1
+      if idx == 1 then
+        sep = ""
+      elseif idx == length then
+        sep = " or "
+      else
+        sep = ", "
+      end
+      ext_string = string.format("%s%s%s%s", ext_string, sep, dirnome, v)
+    end
+    return ext_string
   end
-  return ext_string
 end
 
-local function get_writable_file(sentiero, dirnome, ext_inpt)
+local function get_writable_file(sentiero, slash, dirnome, ext_inpt)
   if is_table(ext_inpt) then
     for _, v in ipairs(ext_inpt) do
-      local new_path = concatenare_sentiero(sentiero, dirnome, v)
+      local new_path = concatenare_sentiero(sentiero, slash, dirnome, v)
       if check_is_writable_file(new_path) then
         return new_path
       end
     end
     return false
   elseif is_string(ext_inpt) then
-    local new_path = concatenare_sentiero(sentiero, dirnome, ext_inpt)
+    local new_path = concatenare_sentiero(sentiero, slash, dirnome, ext_inpt)
     if check_is_writable_file(new_path) then
       return new_path
     end
@@ -138,6 +157,10 @@ local function get_writable_file(sentiero, dirnome, ext_inpt)
   else
     return false
   end
+end
+
+local function print_speared_to(new_nome)
+  print("spear speared to " .. new_nome)
 end
 
 -- end getting detail functions
@@ -152,65 +175,55 @@ function M.spear(ext_input)
   end
 
   local current_name = get_current_name()
+  local slash = get_slash(current_name)
   local is_file_or_dir = check_current_is_file_or_dir(current_name)
 
   if is_file_or_dir == nil then
-    return print("spear: cannot do things here")
+    return print("spear can't do things here")
   end
 
   if is_file_or_dir == "file" then
 
-    local filename = get_last_sec_of_path(current_name)
-    local directory = remove_file_from_dir(current_name, filename)
-    local dir_name = get_last_sec_of_path(directory)
+    local filename = get_last_sec_of_path(current_name, slash)
+    local directory = remove_file_from_dir(current_name, slash, filename)
+    local dir_name = get_last_sec_of_path(directory, slash)
     local extension = remove_from(filename, dir_name)
 
     if ext_input_matches_extension(ext_input, extension) then
       return print("spear: already in file")
     end
 
-    local new_path = get_writable_file(directory, dir_name, ext_input)
+    local new_path = get_writable_file(directory, slash, dir_name, ext_input)
 
     if new_path == false then
-      local ext_string = get_ext_as_string(ext_input)
-      return print(string.format("spear: file with extension %s doesn't exist", ext_string))
+      local ext_string = get_ext_as_string(dir_name, ext_input)
+      return print(string.format("spear can't find %s here", ext_string))
     end
 
     local id = get_buf_to_go_to_id(new_path)
-
+    local new_name = get_last_sec_of_path(new_path, slash)
     change_file(id)
+    print_speared_to(new_name)
 
   elseif is_file_or_dir == "dir" then
 
-    local dirname = get_last_sec_of_path(current_name)
-    local new_path = get_writable_file(current_name, dirname, ext_input)
+    local dirname = get_last_sec_of_path(current_name, slash)
+    local new_path = get_writable_file(current_name, slash, dirname, ext_input)
 
     if new_path == false then
-      return print("spear: file with that name doesn't exist")
+      local ext_string = get_ext_as_string(dirname, ext_input)
+      return print(string.format("spear can't find %s here", ext_string))
     end
 
     local id = get_buf_to_go_to_id(new_path)
 
+    local new_name = get_last_sec_of_path(new_path, slash)
     change_file(id)
-
+    print_speared_to(new_name)
   end
 end
 
 -- end main function
 
--- start angular implementations and presets
--- app specific
-M.spear_mod = function() M.spear(".module.ts") end
-M.spear_route = function() M.spear("-routing.module.ts") end
-
--- component specific
-M.spear_ts = function() M.spear(".component.ts") end
-M.spear_html = function() M.spear(".component.html") end
-M.spear_spec = function() M.spear(".component.spec.ts") end
-M.spear_css = function() M.spear({ ".component.css", ".component.scss" }) end
-
--- combos of unlikely pairs
-M.spear_ts_pipe = function() M.spear({ ".component.ts", ".pipe.ts" }) end
--- end angular implementations and presets
 
 return M
