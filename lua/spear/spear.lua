a0ab2e08cd09fd7bd92615c7080dde6f555d672a
kbario <kylebario1@gmail.com>
2022-10-03 16:33:13 +0800

    refactor to use fnamemodify

--- lua/spear/spear.lua
+++ lua/spear/spear.lua
@@ -65,10 +65,16 @@ local function get_end(buf_nome)
   if not utils.is_string(buf_nome)then
     return false
   end
-  local end_name = string.match(buf_nome:reverse(), "[^".. utils.get_slash() .."]+"):reverse()
+  local filenome = vim.fn.fnamemodify(buf_nome, ":p:t")
+  if not utils.is_string(filenome) then
+    filenome = nil
+  end
+  local dirnome = vim.fn.fnamemodify(buf_nome, ":p:h:t")
+  local new_file_name = vim.fn.fnamemodify(buf_nome, ":p:h:h")
+  return filenome, dirnome, new_file_name
+  --[[ local end_name = string.match(buf_nome:reverse(), "[^".. utils.get_slash() .."]+"):reverse()
   local new_file_name = string.gsub(buf_nome, utils.get_slash()..end_name, "")
-  return end_name, new_file_name
-  -- return vim.fn.fnamemodify(buf_nome, ":t")
+  return end_name, new_file_name ]]
 end
 
 local function get_ext_as_string(dirnome, ext_inpt)
@@ -117,30 +123,30 @@ local function get_path(settings, current_path, dirnome, ext, filenome)
     local first_or_swap = 'first'
     if new_file == filenome then
       if settings["match_pref"] == "first" then
-        return "stay"
+        return "stay", nil
       elseif settings["match"] == "swap" then
         first_or_swap = "swap"
       end
     else
       local new_path = make_new_path(current_path, dirnome, new_file)
       if is_writable_file(new_path.filename) then
-        return new_path.filename, first_or_swap
+        return first_or_swap, new_path.filename
       end
     end
   else
-    return nil
+    return nil, nil
   end
 end
 
 local function get_writable_file(settings, current_path, dirnome, ext_inpt, filenome)
   if utils.is_table(ext_inpt) then
     for _, v in ipairs(ext_inpt) do
-      local new_path = get_path(settings, current_path, dirnome, v, filenome)
+      local action, new_path = get_path(settings, current_path, dirnome, v, filenome)
       if not utils.is_nil(new_path) then
-        return new_path
+        return action, new_path
       end
     end
-    return nil
+    return nil, nil
   else
     return get_path(settings, current_path, dirnome, ext_inpt, filenome)
   end
@@ -190,11 +196,6 @@ function M.spear(ext_input, overrides)
 
   local prefs = utils.validate_options(overrides or {}, false)
   local cur_buf_name = get_abs_buf_name()
-  local buf_name
-  local file_name
-  local dir_name
-  local new_path
-  local first_or_swap
 
   -- check if were in a file or folder
   local is_file_or_dir = check_current_is_file_or_dir(cur_buf_name)
@@ -202,34 +203,27 @@ function M.spear(ext_input, overrides)
     return print("spear can't do things here")
   end
 
-  if is_file_or_dir == "file" then
+  local file_name, dir_name, buf_name = get_end(cur_buf_name)
 
-    file_name, buf_name = get_end(cur_buf_name)
+  local action, new_path = get_writable_file(prefs, buf_name, dir_name, ext_input, file_name)
+
+  --[[ if is_file_or_dir == "file" then
+
+    file_name, dir_name, buf_name = get_end(cur_buf_name)
     if not file_name then return print("spear: invalid buf name") end
 
     dir_name, buf_name = get_end(buf_name)
-    if not dir_name then return print("spear: invalid buf name") end
+    if not dir_name then return print("spear: invalid buf name") end 
 
     new_path, first_or_swap = get_writable_file(prefs, buf_name, dir_name, ext_input, file_name)
 
   elseif is_file_or_dir == "dir" then
+
+    file_name, dir_name, buf_name = get_end(cur_buf_name)
     dir_name, buf_name = get_end(cur_buf_name)
-    if not dir_name then return print("spear: invalid buf name") end
+    if not dir_name then return print("spear: invalid buf name") end 
 
-    new_path, first_or_swap = get_writable_file(prefs, buf_name, dir_name, ext_input, nil)
-  end
-
-  if new_path == "stay" then
-    return already_in(file_name)
-  end
-
-  if new_path == nil then
-    local ext_string = get_ext_as_string(dir_name, ext_input)
-    return print(string.format("spear: no files named %s found", ext_string))
-  end
-
-  --[[ if prefs["save_on_spear"] then
-    vim.api.nvim_command(":w")
+    new_path, first_or_swap = get_writable_file(prefs, buf_name, dir_name, ext_input, file_name)
   end ]]
 
   log.info(
@@ -242,12 +236,25 @@ function M.spear(ext_input, overrides)
     "\nfile name:", file_name,
     "\ndir name:", dir_name,
     "\nnew path:", new_path,
-    "\nfirst or swap:", first_or_swap,
+    "\nfirst or swap:", action,
     "\n"
   )
 
+  if action == "stay" then
+    return already_in(file_name)
+  end
+
+  if utils.is_nil(new_path) then
+    local ext_string = get_ext_as_string(dir_name, ext_input)
+    return print(string.format("spear: no files named %s found", ext_string))
+  end
+
+  if prefs["save_on_spear"] then
+    vim.api.nvim_command(":w")
+  end
+
   change_to(new_path)
-  speared_to(new_path, first_or_swap)
+  speared_to(new_path, action)
 end
 
 -- end main function
